1a2ec358cc8eefab61a1b41967f0a4df40577bf7
Matej Delincak <55759748+mateju25@users.noreply.github.com>
2021-03-26 10:58:52 +0100

    Verification by email implemented in gui

--- src/project/gui/controllers/RegistrationSceneController.java
+++ src/project/gui/controllers/RegistrationSceneController.java
@@ -1,11 +1,13 @@
 package project.gui.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import project.model.loginSystem.PlayerDatabase;
 
 import java.io.IOException;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,15 +19,21 @@ public class RegistrationSceneController {
     @FXML
     private TextField email;
     @FXML
+    private TextField textPin;
+    @FXML
     private Label warning;
+
+    private int generatedPin = 0;
+
     @FXML
     private void registration() {
         warning.setText("");
         String name = this.name.getText();
         String password= this.password.getText();
         String email= this.email.getText();
+        String pin= this.textPin.getText();
 
-        if(name.isEmpty()||password.isEmpty()) {
+        if(name.isEmpty() || password.isEmpty() || pin.isEmpty()) {
             warning.setText("Vyplnťe všetky povinné polia!");
             return;
         }
@@ -40,6 +48,11 @@ public class RegistrationSceneController {
             warning.setText("Daný užívateľ už existuje!");
             return;
         }
+        if (!(String.valueOf(generatedPin).equals(pin)))  {
+            warning.setText("Nesprávny PIN!");
+            return;
+        }
+
         PlayerDatabase.getInstance().registrationUser(name,password,email);
 
         this.name.setText("");
@@ -50,5 +63,17 @@ public class RegistrationSceneController {
     @FXML
     private void changeSceneLogin() throws IOException {
         LoginSceneController.switchScene("/project/gui/views/LoginScene.fxml");
+    }
+
+    public void sendVerificationMail(ActionEvent actionEvent) {
+        Random rand = new Random();
+        generatedPin = rand.nextInt(1000) * 9 + 1;
+        Pattern p = Pattern.compile(".*@.*[.].*");
+        Matcher m = p.matcher(email.getText());
+        if (!m.matches()) {
+            warning.setText("Email v zlom tvare!");
+            return;
+        }
+        PlayerDatabase.getInstance().sendWelcomeEmail(email.getText(), generatedPin);
     }
 }
