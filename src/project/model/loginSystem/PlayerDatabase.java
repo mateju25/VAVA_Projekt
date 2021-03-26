1b0c6f03dd3144d8916ddc0305ba739b49fd16ae
Matej Delincak <55759748+mateju25@users.noreply.github.com>
2021-03-26 09:55:32 +0100

    email verifikacia

--- src/project/model/loginSystem/PlayerDatabase.java
+++ src/project/model/loginSystem/PlayerDatabase.java
@@ -1,6 +1,10 @@
 package project.model.loginSystem;
 
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
 import java.sql.*;
+import java.util.Properties;
 
 public class PlayerDatabase {
     private static PlayerDatabase single_instance = null;
@@ -78,5 +82,38 @@ public class PlayerDatabase {
             statement.executeUpdate("INSERT INTO users VALUES ('" + name +"', '" + password + "', '" + email + "')");
             connection.close();
         } catch (SQLException a) {}
+    }
+
+    public void sendWelcomeEmail(String email, int pin) {
+        String to = email;
+        String from = "voidchess.fiit@gmail.com";
+        String host = "smtp.gmail.com";
+
+        Properties properties = System.getProperties();
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+
+                return new PasswordAuthentication("voidchess.fiit@gmail.com", "28qpj2C5zXTeruiyyBDHD45");
+
+            }
+
+        });
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("Vitaj vo void chesse !");
+            message.setText("Pre úspešné zaregistrovanie zadaj tento PIN do aplikácie: " + String.valueOf(pin));
+            Transport.send(message);
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
     }
 }
